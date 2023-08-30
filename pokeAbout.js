1f0c68b9d957775e4b549e028ae1025459c15ed6
Pamela Raiane <pamelaprpn@gmail.com>
2023-08-30 20:58:43 -0300

    inicio da consulta pra exibir as informações de cada pokemon

--- pokeAbout.js
+++ pokeAbout.js
@@ -3,7 +3,7 @@
     const request = new Request('https://pokeapi.co/api/v2/pokemon/')
     const config = {method: 'get'}
     const dfrag = document.createDocumentFragment()
-    const ul = document.getElementById('listaPokemon')
+    
    
 
     const listPoke = await fetch(request, config)
@@ -16,10 +16,8 @@
     listPoke.map(function(pokemon){
         let li = document.createElement('li')
         let ancora = document.createElement('a')
-        console.log(li)
-        console.log(ancora)
 
-        ancora.setAttribute('href', `${pokemon.url}`)
+        ancora.setAttribute('href', 'pokeAbout.html')
         ancora.innerHTML = `${pokemon.name}` 
 
         li.appendChild(ancora)  
@@ -29,6 +27,27 @@
     })
 
     document.getElementById('listaPokemon').appendChild(dfrag)
+
+    for(let i = 1; i <= listPoke.length; i++){
+       let teste = `https://pokeapi.co/api/v2/pokemon/${[i]}`;
+       console.log(teste)
+
+       const listAboutPokemon = await fetch(teste, config)
+       .then((data) => data.json() 
+       .then(data => data.results))
+       .catch((err) => {
+        console.log(err)
+       })
+
+       listAboutPokemon.map(function(aboutPokemon){
+        let liPokemon = document.createElement('li')
+        
+
+        console.log(liPokemon)
+        
+
+       })
+    }
     
 
 })()
